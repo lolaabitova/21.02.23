e4d2c0594f9527333fe6ed230a836a0c90513412
Lola <lolaabitova90@gmail.com>
2023-02-21 14:50:40 +0600

    selected multiple items in ntml index and synced with js

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-console.log("Hello")
+
+let example = document.querySelector("p");
+console.log(example);
+let example1 = document.querySelector("article");
+console.log(example1);
+let example3 = document.querySelector(".example-3");
+console.log(example3);
+let example4 = document.querySelector("#example-4");
+console.log(example4);
+let example5 = document.querySelector("p:nth-child(4)");
+console.log(example5);
